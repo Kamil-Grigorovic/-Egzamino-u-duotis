ba8997f304834c5c686315fa996159de4f542653
Kamil-Grigorovic <kamil.grigorovic@gmail.com>
2025-12-23 17:48:19 +0200

    Add includes and extern declarations in header.h

--- header.h
+++ header.h
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <set>
+#include <sstream>
+#include <cctype>
 
+using namespace std;
+
+extern map<string, int> wordCount;
+extern map<string, set<int>> wordLines;
+extern set<string> urls;
